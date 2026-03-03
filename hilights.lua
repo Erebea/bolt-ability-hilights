9c2e0f04101377d5b8b44262cad7c353dec425ab
erebea <erebeadeity@gmail.com>
2026-03-02 19:40:46 -0500

    fixed hilight script

--- hilights.lua
+++ hilights.lua
@@ -4,7 +4,6 @@ drawhilights = function()
   abilities.sonic.hilight   = buffs.runiccharge.active
   abilities.conc.hilight    = buffs.runiccharge.active
   abilities.dbreath.hilight = (buffs.runiccharge.active or buffs.combust.active) and not abilities.dbreath.isoncd
-  abilities.combust.hilight = true
 
   abilities.assault.hilight = buffs.bloodlust.active and (buffs.bloodlust.number or 0) >= 4 or false
   abilities.flurry.hilight = buffs.bloodlust.active and (buffs.bloodlust.number or 0) >= 4 or false
@@ -19,7 +18,7 @@ drawhilights = function()
   abilities.volley.hilight = buffs.residualsouls.active and ((equipment.t95lantern.x and (buffs.residualsouls.parensnumber or buffs.residualsouls.number or 0) == 5) or (not equipment.t95lantern.x and (buffs.residualsouls.parensnumber or buffs.residualsouls.number or 0) == 3))
   abilities.cmdzom.hilight = buffs.putridzombie.active and (buffs.putridzombie.number or 0) <=5 or false
   abilities.cmdphant.hilight = buffs.phantomguardian.active and (buffs.phantomguardian.parensnumber or 0) >= 13 or false
-  abilities.skulls.hilight = not abilities.skulls.isoncd
+  abilities.skulls.hilight = buffs.livingdeath.active and not abilities.skulls.isoncd
   abilities.necroauto.hilight = buffs.deathspark.active
 
   hilightabilities()
